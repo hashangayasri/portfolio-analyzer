0e03ae4adb61e29d0519842c54d80b031f2a11d7
Hashan Gayasri Udugahapattuwa <hashan.gayasri@gmail.com>
2021-12-11 16:38:09 +0530

    Add multiple filtering options + refinements + refactoring

--- pf-analyzer.py
+++ pf-analyzer.py
@@ -13,6 +13,11 @@ import argparse
 account_file = "Account"
 pf_file = "Portfolio.xlsx"
 split_info_file = "splits.csv"
+skip_split_info_calculation=False
+skip_quotes_file_generation=False
+ignore_portfolio_last_price=False
+print_qty_mismatched_symbol_info=False
+ignore_filtered_out_transactions_for_price=False
 
 parser = argparse.ArgumentParser(description='Provides an insight into stock portfolios')
 parser.add_argument('tx_filter_end_date' , type=np.datetime64, nargs='?', default=None,
@@ -25,8 +30,25 @@ parser.add_argument('--pf_file', default=pf_file, metavar=pf_file,
                     help='Current Portfolio file containing last prices [Optional]')
 parser.add_argument('--split_info_file', default=split_info_file, metavar=split_info_file,
                     help='Split info file. Approximate split info file will be generated automatically.')
+parser.add_argument('--skip_split_info_calculation', dest='skip_split_info_calculation', default=skip_split_info_calculation, action='store_true',
+                    help='Skip deducing split info.')
+parser.add_argument('--skip_quotes_file_generation', dest='skip_quotes_file_generation', default=skip_quotes_file_generation, action='store_true',
+                    help='Skip generating the Yahoo Finance compatible quotes file.')
+parser.add_argument('--ignore_portfolio_last_price', dest='ignore_portfolio_last_price', default=ignore_portfolio_last_price, action='store_true',
+                    help='By default, last price from the portfolio is also used to update the last price. '
+                    'When this is set, only the last transaction value will be used as the last price.')
+parser.add_argument('--print_qty_mismatched_symbol_info', dest='print_qty_mismatched_symbol_info', default=print_qty_mismatched_symbol_info, action='store_true',
+                    help='Print infomation about symbols with quantity mismatches.')
+parser.add_argument('--ignore_filtered_out_transactions_for_price', dest='ignore_filtered_out_transactions_for_price', default=ignore_filtered_out_transactions_for_price, action='store_true',
+                    help='By default, even the transactions outside the filter range are used to update the last price. '
+                    'When this is set, only the transactions within the filtered range will be considered.')
+parser.add_argument('--no_buy_after_date' , type=np.datetime64, nargs='?', default=None,
+                    help='If set, ignore buy transactions after this date')
+parser.add_argument('--day_reports', type=np.datetime64, required=False, action='append', # nargs='*',
+                    help='Get day report for the given date(s)')
 args = parser.parse_args()
 
+print("Program Options:")
 for variable, value in parser.parse_args()._get_kwargs():
     if value is not None:
         print("{} = {}".format(variable, value))
@@ -36,6 +58,13 @@ tx_filter_start_date=args.tx_filter_start_date
 account_file=args.account_file
 pf_file=args.pf_file
 split_info_file=args.split_info_file
+skip_split_info_calculation=args.skip_split_info_calculation
+skip_quotes_file_generation=args.skip_quotes_file_generation
+ignore_portfolio_last_price=args.ignore_portfolio_last_price
+print_qty_mismatched_symbol_info=args.print_qty_mismatched_symbol_info
+ignore_filtered_out_transactions_for_price=args.ignore_filtered_out_transactions_for_price
+no_buy_after_date=args.no_buy_after_date
+day_reports=args.day_reports
 
 generated_split_info_file = "gen-" + split_info_file
 generated_poss_split_info_file = "gen-poss-" + split_info_file
@@ -117,39 +146,38 @@ pd.options.mode.chained_assignment = None  # default=='warn' # TODO: Fix this
 tx['Symbol'] = tx['Instrument']
 setInstrument(tx)
 
-per_symbol = tx.groupby('Symbol')
 splits_list = []
-
-
 def addSplit(poss_split_date, symbol, poss_split_ratio):
     split = pd.DataFrame(data={'Date': poss_split_date, "Symbol": symbol, "Ratio": poss_split_ratio}, index=[0])
     splits_list.append(split)
 
+def calculateSplitInfo():
+    per_symbol = tx.groupby('Symbol')
+    for symbol, transactions in per_symbol:
+        tx_iter = transactions.iterrows()
+        last_price = next(tx_iter)[1]['Price']
+        for _, row in tx_iter:
+            if last_price / row['Price'] > 1.75:
+                poss_split_ratio = round(last_price / row['Price'])
+                print("Possible split in {} of 1:{} before {}".format(symbol, poss_split_ratio, getFormattedDate(row['Date'])))
+                poss_split_date = row['Date'] - pd.Timedelta(days=1)
+                split_info = []
+                addSplit(poss_split_date, symbol, poss_split_ratio)
+            last_price = row['Price']
+    print()
 
 def writeSplitInfoToFile(si_file):
     if splits_list:
-        pd.concat(splits_list, ignore_index=True).to_csv(si_file, index=False)
+        pd.concat(splits_list, ignore_index=True).to_csv(si_file, index=False)    
+    if not path.exists(split_info_file):
+        copyfile(si_file, split_info_file)
+        print("Split info file {} not found. Copying {}... Please inspect the split info file for any errors.".format(
+            split_info_file, si_file))
 
+if not skip_split_info_calculation:
+    calculateSplitInfo()
+    writeSplitInfoToFile(generated_split_info_file)
 
-for symbol, transactions in per_symbol:
-    tx_iter = transactions.iterrows()
-    last_price = next(tx_iter)[1]['Price']
-    for _, row in tx_iter:
-        if last_price / row['Price'] > 1.75:
-            poss_split_ratio = round(last_price / row['Price'])
-            print("Possible split in {} of 1:{} before {}".format(symbol, poss_split_ratio, row['Date']))
-            poss_split_date = row['Date'] - pd.Timedelta(days=1)
-            split_info = []
-            addSplit(poss_split_date, symbol, poss_split_ratio)
-        last_price = row['Price']
-print()
-
-writeSplitInfoToFile(generated_split_info_file)
-
-if not path.exists(split_info_file):
-    copyfile(generated_split_info_file, split_info_file)
-    print("Split info file {} not found. Copying {}... Please inspect the split info file for any errors.".format(
-        split_info_file, generated_split_info_file))
 
 splits = pd.read_csv(split_info_file)
 # splits['Date'] = pd.to_datetime(splits['Date'], format='%Y-%m-%d')
@@ -175,11 +203,14 @@ if tx_filter_end_date:
 if tx_filter_start_date:
     print("Filtering out transactions made before : {}".format(tx_filter_start_date))
     tx_filtered = tx_filtered[tx_filtered['Date'] >= tx_filter_start_date]
+if no_buy_after_date:
+    print("Filtering out buy transactions made after : {}".format(no_buy_after_date))
+    tx_filtered = tx_filtered[(tx_filtered['Date'] <= no_buy_after_date) | (tx_filtered['Qty'] <= 0)]
 
 qty_amount = tx_filtered[['Instrument', 'Qty', 'Amount']].groupby('Instrument', as_index=False).sum()
 qty_amount['PPS'] = qty_amount['Amount'] / qty_amount['Qty']
 
-last_traded_price = tx[['Instrument', 'Price']].groupby('Instrument').last().to_dict()['Price']
+last_traded_price = (tx_filtered if ignore_filtered_out_transactions_for_price else tx)[['Instrument', 'Price']].groupby('Instrument').last().to_dict()['Price']
 
 pf = None
 pf_price = {}
@@ -187,7 +218,6 @@ sales_commission = 1.01133
 qty_mismatched_symbols = []
 
 if path.exists(pf_file):
-    mismatched_symbols_found = False
     pf = pd.read_excel(pf_file, header=2, parse_dates=True)
     pf = pf.dropna()
     pf['Symbol'] = pf['Security'].str.extract(r'(?P<Instrument>[A-Z.]*)')
@@ -195,64 +225,84 @@ if path.exists(pf_file):
     setInstrument(pf)
     pf_price = pf[['Instrument', 'Traded Price']].groupby('Instrument').last().to_dict()['Traded Price']
     sales_commission = (pf['Traded Price'] / pf['Sell Price'])[0]
-    qty_amount_ins = qty_amount
-    qty_amount_ins['Symbol'] = qty_amount_ins['Instrument'].map(lambda i: mapped_symbol[i])
-    qty_amount_pf = pd.merge(qty_amount_ins, pf, on=['Symbol'], validate="1:1")
-    pf_quantities = qty_amount_pf[['Symbol', 'Qty', 'Quantity']]
-    mismatched_pf_quantities = pf_quantities[pf_quantities['Qty'] != pf_quantities['Quantity']]
-    for _, row in mismatched_pf_quantities.iterrows():
-        mismatched_symbols_found = True
-        print("Quantity mismatch in {}. Possible misconfigured split - Quantities are {} vs {}".format(row['Symbol'],
-                                                                                                       row['Qty'],
-                                                                                                       row['Quantity']))
-        poss_split_ratio_approx = row['Quantity'] / row['Qty']
-        if (poss_split_ratio_approx == round(poss_split_ratio_approx)):
-            last_date = tx[tx['Symbol'] == row['Symbol']].groupby('Symbol', as_index=False).last()['Date'][0]
-            print("Possible recent split of 1:{} in {} after {}".format(round(poss_split_ratio_approx), row['Symbol'],
-                                                                        last_date))
-            addSplit(last_date, row['Symbol'], round(poss_split_ratio_approx))
-        qty_mismatched_symbols.append(row['Symbol'])
-    writeSplitInfoToFile(generated_poss_split_info_file)
+    if not skip_split_info_calculation:
+        qty_amount_ins = qty_amount
+        qty_amount_ins['Symbol'] = qty_amount_ins['Instrument'].map(lambda i: mapped_symbol[i])
+        # with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        #     print(pf)
+        qty_amount_pf = pd.merge(qty_amount_ins, pf, on=['Symbol'])
+        with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+            print(qty_amount_pf)
+        qty_amount_pf = pd.merge(qty_amount_ins, pf, on=['Symbol'], validate="1:1")
+        pf_quantities = qty_amount_pf[['Symbol', 'Qty', 'Quantity']]
+        mismatched_pf_quantities = pf_quantities[pf_quantities['Qty'] != pf_quantities['Quantity']]
+        for _, row in mismatched_pf_quantities.iterrows():
+            print("Quantity mismatch in {}. Possible misconfigured split or scrip dividend - Quantities are {} vs {}".format(row['Symbol'],
+                                                                                                        row['Qty'],
+                                                                                                        row['Quantity']))
+            poss_split_ratio_approx = row['Quantity'] / (row['Qty'] if row['Qty'] !=0 else 1)
+            if (poss_split_ratio_approx == round(poss_split_ratio_approx)):
+                last_date = tx[tx['Symbol'] == row['Symbol']].groupby('Symbol', as_index=False).last()['Date'][0]
+                print("Possible recent split of 1:{} in {} after {}".format(round(poss_split_ratio_approx), row['Symbol'],
+                                                                            last_date))
+                addSplit(last_date, row['Symbol'], round(poss_split_ratio_approx))
+            qty_mismatched_symbols.append(row['Symbol'])
+        writeSplitInfoToFile(generated_poss_split_info_file)
     txp = tx
     txp['Approx. share value'] = txp['Amount'] / txp['Qty']
-    for mmi in qty_mismatched_symbols:
-        print("\nMismatched Symbol : {}".format(mmi))
-        print(pf[pf['Symbol'] == mmi].to_string())
-        print(qty_amount[qty_amount['Symbol'] == mmi].to_string())
-        print(txp[txp['Symbol'] == mmi].to_string())
-        print("\n")
-    if not mismatched_symbols_found:
+    if qty_mismatched_symbols:
+        for mmi in qty_mismatched_symbols:
+            if print_qty_mismatched_symbol_info:
+                print("\nMismatched Symbol : {}".format(mmi))
+                print(pf[pf['Symbol'] == mmi].to_string())
+                print(qty_amount[qty_amount['Symbol'] == mmi].to_string())
+                print(txp[txp['Symbol'] == mmi].to_string())
+                print("\n")
+        if not print_qty_mismatched_symbol_info:
+            print("Quantity mismatches found. Run with --print_qty_mismatched_symbol_info to get additional information.\n")
+    else:
         print("No quantity mismatches found. Split info is likely up to date.\n")
 
-    # Dump Quotes file to match the Yahoo Finance import format
-    # pff=pd.DataFrame()
-    # pff['Symbol']=tx['Symbol'].map(lambda s: s.replace('.','') + '0000.CM')
-    # pff['Date']=tx['Date'].dt.strftime('%Y/%m/%d')
-    # pff['Time']='14:29 IST'
-    # pff['Trade Date']=tx['Date'].dt.strftime('%Y%m%d')
-    # pff['Purchase Price']=tx['Price']
-    pff = pd.DataFrame()
-    pff['Symbol'] = pf['Security'].map(lambda s: s.replace('.', '') + '.CM')
-    pff['Date'] = datetime.today().strftime('%Y/%m/%d')
-    pff['Time'] = '14:29 IST'
-    pff['Trade Date'] = datetime.today().strftime('%Y%m%d')
-    pff['Purchase Price'] = pf['Avg Price']
-    pff['Quantity'] = pf['Quantity']
-    pff['Commission'] = (pf['B.E.S Price'] - pf['Avg Price']) * pf['Quantity']
-    pff.sort_values('Commission').to_csv("Quotes.csv")
+    if not skip_quotes_file_generation:
+        # Dump Quotes file to match the Yahoo Finance import format
+        # pff=pd.DataFrame()
+        # pff['Symbol']=tx['Symbol'].map(lambda s: s.replace('.','') + '0000.CM')
+        # pff['Date']=tx['Date'].dt.strftime('%Y/%m/%d')
+        # pff['Time']='14:29 IST'
+        # pff['Trade Date']=tx['Date'].dt.strftime('%Y%m%d')
+        # pff['Purchase Price']=tx['Price']
+        pff = pd.DataFrame()
+        pff['Symbol'] = pf['Security'].map(lambda s: s.replace('.', '') + '.CM')
+        pff['Date'] = datetime.today().strftime('%Y/%m/%d')
+        pff['Time'] = '14:29 IST'
+        pff['Trade Date'] = datetime.today().strftime('%Y%m%d')
+        pff['Purchase Price'] = pf['Avg Price']
+        pff['Quantity'] = pf['Quantity']
+        pff['Commission'] = (pf['B.E.S Price'] - pf['Avg Price']) * pf['Quantity']
+        pff.sort_values('Commission').to_csv("Quotes.csv")
 else:
     print("\nPortfolio file {} does not exist. Save the Portfolio file as well for more accurate results\n".format(
         pf_file))
 
-last_price = {**last_traded_price, **pf_price}
+last_price =  {**last_traded_price} if ignore_portfolio_last_price else {**last_traded_price, **pf_price}
 
 tx_filtered['Eff. Price'] = tx_filtered['Amount'] / tx_filtered['Qty']
 tx_filtered['Last Price'] = tx_filtered['Instrument'].map(lambda i: last_price[i])
 tx_filtered['Last Price - Effective'] = tx_filtered['Last Price'] * tx_filtered['Amount'].map(
     lambda a: sales_commission if a >= 0 else 2 - sales_commission)
-tx_filtered_bs = tx_filtered.groupby(['Instrument', 'TX_Type']).agg(
-    {'Qty': 'sum', 'Amount': 'sum', 'Last Price': 'last', 'Last Price - Effective': 'last'})
-tx_filtered_bs.insert(2, 'Avg. Price', tx_filtered_bs['Amount'] / tx_filtered_bs['Qty'])
+
+def buySellAggregate(groupby):
+    bs = groupby.agg(
+        {'Qty': 'sum', 'Amount': 'sum', 'Last Price': 'last', 'Last Price - Effective': 'last'})
+    bs.insert(2, 'Avg. Price', bs['Amount'] / bs['Qty'])
+    return bs
+
+if day_reports:
+    print("Day Reports:")
+    day_report = buySellAggregate(tx_filtered[tx_filtered['Date'].isin(day_reports)].groupby(['Date', 'Instrument', 'TX_Type']))
+    print(day_report.to_string())
+
+tx_filtered_bs = buySellAggregate(tx_filtered.groupby(['Instrument', 'TX_Type']))
 print(tx_filtered_bs.to_string())
 print()
 
